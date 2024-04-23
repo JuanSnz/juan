cb3df063bb76753158d37fb99c7965299587a752
juanmanuelsanchez0000@gmail.com <juanmanuelsanchez0000@gmail.com>
2024-04-23 09:55:25 -0400

    mi segundo commit

---
+++ static.php
@@ -0,0 +1,19 @@
+<?php 
+$nombre = 'Haland';
+class Persona {
+	public static $dia = '7 de setiembre';
+		
+	public static function saludo($nombre = null){
+		if ($nombre){
+			return 'Hola, buen dia ' . $nombre;
+		}else{
+			return 'Hola, buen dia';
+		}
+	} 
+}
+// $carlos = new Persona;
+// echo $carlos->saludo('Carlos');
+
+
+echo Persona::saludo();
+//como acceder a metodo statico sin hacer otra clases o funcion 
